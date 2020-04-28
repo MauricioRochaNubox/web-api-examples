65e9dda22d7e4c7e4c5a8247bc579947c37a3ae3
MauricioRochaNubox <52832925+MauricioRochaNubox@users.noreply.github.com>
2020-04-28 19:58:07 -0400

    creación autenticacion-response-json-example.js

---
+++ autenticacion-response-json-example.js
@@ -0,0 +1,70 @@
+{
+    "Sistemas": [
+        {
+            "Sistema": "SERVIREMU",
+            "NumeroDeSerie": 1,
+            "NroContrato": 60747,
+            "Rut": 15478788
+        },
+        {
+            "Sistema": "SERVICONT PLUS",
+            "NumeroDeSerie": 1,
+            "NroContrato": 60775,
+            "Rut": 15478788
+        },
+        {
+            "Sistema": "CONTABILIDAD",
+            "NumeroDeSerie": 1,
+            "NroContrato": 68718,
+            "Rut": 15478788
+        },
+        {
+            "Sistema": "PERFILAMIENTO",
+            "NumeroDeSerie": 1,
+            "NroContrato": 68953,
+            "Rut": 15478788
+        },
+        {
+            "Sistema": "MINUBOX",
+            "NumeroDeSerie": 1,
+            "NroContrato": 72117,
+            "Rut": 15478788
+        },
+        {
+            "Sistema": "SERVIFACTURA",
+            "NumeroDeSerie": 1,
+            "NroContrato": 76069,
+            "Rut": 15478788
+        },
+        {
+            "Sistema": "ADMINISTRACION COMERCIAL",
+            "NumeroDeSerie": 1,
+            "NroContrato": 77054,
+            "Rut": 15478788
+        },
+        {
+            "Sistema": "ADMINISTRACION COMERCIAL CON FACTURA",
+            "NumeroDeSerie": 1,
+            "NroContrato": 77055,
+            "Rut": 15478788
+        },
+        {
+            "Sistema": "SERVIFACTURA CERT SII",
+            "NumeroDeSerie": 1,
+            "NroContrato": 79043,
+            "Rut": 15478788
+        },
+        {
+            "Sistema": "SERVIFACTURA",
+            "NumeroDeSerie": 2,
+            "NroContrato": 139255,
+            "Rut": 15478788
+        },
+        {
+            "Sistema": "SERVIFACTURA",
+            "NumeroDeSerie": 1,
+            "NroContrato": 138427,
+            "Rut": 7964492
+        }
+    ]
+}
