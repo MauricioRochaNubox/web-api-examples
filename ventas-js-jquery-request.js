e1e849cb99684e61e80aabca73afc0aca051fdc0
MauricioRochaNubox <52832925+MauricioRochaNubox@users.noreply.github.com>
2020-04-28 01:38:03 -0400

    Create ventas-js-jquery-request.js

---
+++ ventas-js-jquery-request.js
@@ -0,0 +1,16 @@
+{
+    "NumeroSerie": 1,
+    "Rut": "7964492-7",
+    "Folio": 5,
+    "CodigoTipoDocumento": 39,
+    "FechaEmision": "2020-04-01T00:00:00.000Z",
+    "MontoNeto": 343330,
+    "MontoExento": 0,
+    "MontoIva": 65223,
+    "MontoTotal": 408563,
+    "Contraparte": {
+        "Rut": "16197839-6",
+        "RazonSocial": "Beta API",
+        "ComunaId": 644
+    }
+}
