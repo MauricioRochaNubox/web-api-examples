599fbf47846ddf838a0c887b6c1ad9aad75fddde
MauricioRochaNubox <52832925+MauricioRochaNubox@users.noreply.github.com>
2020-04-28 20:16:04 -0400

    Update autenticacion-response-json-example.js

--- autenticacion-response-json-example.js
+++ autenticacion-response-json-example.js
@@ -1,18 +1,6 @@
 {
     "Sistemas": [
         {
-            "Sistema": "SERVIREMU",
-            "NumeroDeSerie": 1,
-            "NroContrato": 60747,
-            "Rut": 15478788
-        },
-        {
-            "Sistema": "SERVICONT PLUS",
-            "NumeroDeSerie": 1,
-            "NroContrato": 60775,
-            "Rut": 15478788
-        },
-        {
             "Sistema": "CONTABILIDAD",
             "NumeroDeSerie": 1,
             "NroContrato": 68718,
@@ -28,30 +16,6 @@
             "Sistema": "MINUBOX",
             "NumeroDeSerie": 1,
             "NroContrato": 72117,
-            "Rut": 15478788
-        },
-        {
-            "Sistema": "SERVIFACTURA",
-            "NumeroDeSerie": 1,
-            "NroContrato": 76069,
-            "Rut": 15478788
-        },
-        {
-            "Sistema": "ADMINISTRACION COMERCIAL",
-            "NumeroDeSerie": 1,
-            "NroContrato": 77054,
-            "Rut": 15478788
-        },
-        {
-            "Sistema": "ADMINISTRACION COMERCIAL CON FACTURA",
-            "NumeroDeSerie": 1,
-            "NroContrato": 77055,
-            "Rut": 15478788
-        },
-        {
-            "Sistema": "SERVIFACTURA CERT SII",
-            "NumeroDeSerie": 1,
-            "NroContrato": 79043,
             "Rut": 15478788
         },
         {
