7b16c64bab68a38d7058b532b5a1ef08308ffdf4
MauricioRochaNubox <52832925+MauricioRochaNubox@users.noreply.github.com>
2020-05-05 19:42:16 -0400

    creación compras-json-example
    archivo de ejemplo para importante en la web api

---
+++ compras-json-example.js
@@ -0,0 +1,69 @@
+{
+  "NumeroSerie": 0,
+  "Rut": "string",
+  "RazonSocialProveedor": "string",
+  "CodigoSIITipoDoc": 0,
+  "Folio": 0,
+  "Fecha": "2020-05-05T23:39:16.210Z",
+  "FechaVencimiento": "2020-05-05T23:39:16.210Z",
+  "RutEmisor": "string",
+  "MontoExento": 0,
+  "MontoNeto": 0,
+  "MontoIva": 0,
+  "MontoTotal": 0,
+  "Glosa": "string",
+  "MontoIvaNoRetenido": 0,
+  "IVAUsoComun": 0,
+  "Periodo": 0,
+  "Anno": 0,
+  "TotalLey18211": 0,
+  "TipoTransaccionCompra": "string",
+  "NetoActivoFijo": 0,
+  "IvaActivoFijo": 0,
+  "DocumentoReferencia": [
+    {
+      "FolioReferencia": 0,
+      "CodigoSIITipoDocReferencia": 0,
+      "Fecha": "2020-05-05T23:39:16.211Z"
+    },
+    {
+      "FolioReferencia": 0,
+      "CodigoSIITipoDocReferencia": 0,
+      "Fecha": "2020-05-05T23:39:16.211Z"
+    }
+  ],
+  "OtrosImpuestos": [
+    {
+      "CodigoImpuesto": 0,
+      "MontoImpuesto": 0
+    },
+    {
+      "CodigoImpuesto": 0,
+      "MontoImpuesto": 0
+    }
+  ],
+  "IVANoRecuperable": [
+    {
+      "Codigo": 0,
+      "Monto": 0
+    },
+    {
+      "Codigo": 0,
+      "Monto": 0
+    }
+  ],
+  "ComisionesYOtrosCargos": [
+    {
+      "Neto": 0,
+      "Exento": 0,
+      "Iva": 0,
+      "Total": 0
+    },
+    {
+      "Neto": 0,
+      "Exento": 0,
+      "Iva": 0,
+      "Total": 0
+    }
+  ]
+}
