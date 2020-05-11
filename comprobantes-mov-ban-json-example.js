cbc2d30c25b7023ad3ed80f92e666a86aa671351
MauricioRochaNubox <52832925+MauricioRochaNubox@users.noreply.github.com>
2020-05-11 12:41:43 -0400

    creación comprobantes-mov-ban-json-example.js

---
+++ comprobantes-mov-ban-json-example.js
@@ -0,0 +1,9 @@
+      "MovimientosBancarios": [
+        {
+          "EsDebito": true,
+          "Fecha": "2020-05-11T15:52:53.558Z",
+          "Valor": 0,
+          "Folio": 0,
+          "TipoMovimientoBancario": "string"
+        }
+      ];
