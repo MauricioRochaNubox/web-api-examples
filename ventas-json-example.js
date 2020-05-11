90de5bfeda6510ecfa19847010c1c27da58baaa1
MauricioRochaNubox <52832925+MauricioRochaNubox@users.noreply.github.com>
2020-05-11 10:55:47 -0400

    creación ventas-json-example.js

---
+++ ventas-json-example.js
@@ -0,0 +1,29 @@
+{
+  "NumeroSerie": 0,
+  "Rut": "string",
+  "Folio": 0,
+  "CodigoTipoDocumento": 0,
+  "FechaEmision": "2020-05-11T14:52:37.418Z",
+  "MontoNeto": 0,
+  "MontoExento": 0,
+  "MontoIva": 0,
+  "MontoTotal": 0,
+  "Contraparte": {
+    "Rut": "string",
+    "RazonSocial": "string",
+    "Giro": "string",
+    "ComunaId": 0,
+    "Direccion": "string",
+    "Email": "string"
+  },
+  "Referencia": {
+    "Folio": 0,
+    "CodigoTipoDocumento": 0
+  },
+  "ImpuestosAdicionales": [
+    {
+      "Monto": 0,
+      "CodigoTipoDeImpuesto": 0
+    }
+  ]
+}
