2798e0d478938263f347663c8ac6b2f15707caf2
MauricioRochaNubox <52832925+MauricioRochaNubox@users.noreply.github.com>
2020-05-11 12:37:03 -0400

    creación comprobantes-mov-aux-json-example.js

---
+++ comprobantes-mov-aux-json-example.js
@@ -0,0 +1,10 @@
+  "MovimientosAuxiliares": [
+        {
+          "Folio": 0,
+          "CodigoSII": 0,
+          "EsDebito": true,
+          "FechaVencimiento": "2020-05-11T15:52:53.557Z",
+          "Valor": 0,
+          "RUTContraparteComercial": "string"
+        }
+  ];
