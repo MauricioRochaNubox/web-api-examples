4201e2c32ee1f7b96ee5a6dc508268b4aee4625c
MauricioRochaNubox <52832925+MauricioRochaNubox@users.noreply.github.com>
2020-05-11 09:02:25 -0400

    creación recaudacion-json-example.js

---
+++ recaudacion-json-example.js
@@ -0,0 +1,15 @@
+{
+  "NumeroSerie": 0,
+  "RutContraparteReceptor": "string",
+  "RutEmpresaEmisor": "string",
+  "CodigoMedioDePago": "string",
+  "Fecha": "2020-05-11T12:59:26.320Z",
+  "MontoTotal": 0,
+  "DetalleDocumentos": [
+    {
+      "Folio": 0,
+      "CodigoSIITipoDoc": 0,
+      "Monto": 0
+    }
+  ]
+}
