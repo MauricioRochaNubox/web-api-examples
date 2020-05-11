07316bbfbe97d5393b3817eceabdc1a0fb07ccd7
MauricioRochaNubox <52832925+MauricioRochaNubox@users.noreply.github.com>
2020-05-11 12:49:32 -0400

    cambios de identación

--- comprobantes-mov-ban-json-example.js
+++ comprobantes-mov-ban-json-example.js
@@ -1,9 +1,9 @@
-      "MovimientosBancarios": [
-        {
-          "EsDebito": true,
-          "Fecha": "2020-05-11T15:52:53.558Z",
-          "Valor": 0,
-          "Folio": 0,
-          "TipoMovimientoBancario": "string"
-        }
-      ];
+"MovimientosBancarios": [
+  {
+    "EsDebito": true,
+    "Fecha": "2020-05-11T15:52:53.558Z",
+    "Valor": 0,
+    "Folio": 0,
+    "TipoMovimientoBancario": "string"
+  }
+];
