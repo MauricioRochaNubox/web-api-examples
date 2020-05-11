abf8ae9277c93011e328ceb27d8385eb0ca2f173
MauricioRochaNubox <52832925+MauricioRochaNubox@users.noreply.github.com>
2020-05-11 12:19:39 -0400

    creación comprobantes-movimiento-json-example.js

---
+++ comprobantes-movimiento-json-example.js
@@ -0,0 +1,40 @@
+  "MovimientosContables": [
+    {
+      "Descripcion": "string",
+      "CodigoCuenta": "string",
+      "EsDebito": true,
+      "Valor": 0,
+      "CodigoCentroDeCosto": "string",
+      "CodigoSucursal": "string",
+      "MovimientosAuxiliares": [
+        {
+          "Folio": 0,
+          "CodigoSII": 0,
+          "EsDebito": true,
+          "FechaVencimiento": "2020-05-11T15:52:53.557Z",
+          "Valor": 0,
+          "RUTContraparteComercial": "string"
+        }
+      ],
+      "MovimientosBancarios": [
+        {
+          "EsDebito": true,
+          "Fecha": "2020-05-11T15:52:53.558Z",
+          "Valor": 0,
+          "Folio": 0,
+          "TipoMovimientoBancario": "string"
+        }
+      ],
+      "BoletasDeHonorarios": [
+        {
+          "Folio": 0,
+          "FechaEmision": "2020-05-11T15:52:53.558Z",
+          "Descripcion": "string",
+          "RUTContraparteComercial": "string",
+          "ValorNeto": 0,
+          "CodigoSII": 0,
+          "EsDebito": true
+        }
+      ]
+    }
+  ]
