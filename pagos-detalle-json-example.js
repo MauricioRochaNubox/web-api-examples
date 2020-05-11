6aa1c39575058c2ab060cebcf74c7a9d521555fb
MauricioRochaNubox <52832925+MauricioRochaNubox@users.noreply.github.com>
2020-05-11 09:17:25 -0400

    creación pagos-detalle-json-example.js

---
+++ pagos-detalle-json-example.js
@@ -0,0 +1,12 @@
+  "DetalleDocumentos": [
+    {
+      "Folio": 1,
+      "CodigoSIITipoDoc": 33,
+      "Monto": 1000
+    },
+    {
+      "Folio": 2,
+      "CodigoSIITipoDoc": 33,
+      "Monto": 1500
+    }
+  ]
