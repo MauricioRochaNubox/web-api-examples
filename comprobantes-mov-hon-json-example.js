cb4e0e7404ba16b3fecb250a8c0312f0c4ac29d9
MauricioRochaNubox <52832925+MauricioRochaNubox@users.noreply.github.com>
2020-05-11 12:44:56 -0400

    creación comprobantes-mov-hon-json-example.js

---
+++ comprobantes-mov-hon-json-example.js
@@ -0,0 +1,11 @@
+      "BoletasDeHonorarios": [
+        {
+          "Folio": 0,
+          "FechaEmision": "2020-05-11T15:52:53.558Z",
+          "Descripcion": "string",
+          "RUTContraparteComercial": "string",
+          "ValorNeto": 0,
+          "CodigoSII": 0,
+          "EsDebito": true
+        }
+      ]
