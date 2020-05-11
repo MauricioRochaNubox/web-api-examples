f94774c2dea115315026bd6dec4b6ee90e8424d8
MauricioRochaNubox <52832925+MauricioRochaNubox@users.noreply.github.com>
2020-05-11 09:09:40 -0400

    creación pagos-json-example.js

---
+++ pagos-json-example.js
@@ -0,0 +1,15 @@
+{
+  "NumeroSerie": 0,
+  "RutContraparteEmisor": "string",
+  "RutEmpresaReceptor": "string",
+  "CodigoMedioDePago": "string",
+  "Fecha": "2020-05-11T13:08:44.556Z",
+  "MontoTotal": 0,
+  "DetalleDocumentos": [
+    {
+      "Folio": 0,
+      "CodigoSIITipoDoc": 0,
+      "Monto": 0
+    }
+  ]
+}
