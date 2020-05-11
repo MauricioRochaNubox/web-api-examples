0c707011c96357e25c736be60bf8d89b4c26ee95
MauricioRochaNubox <52832925+MauricioRochaNubox@users.noreply.github.com>
2020-05-11 12:01:28 -0400

    creación comprobantes-json-example.js

---
+++ comprobantes-json-example.js
@@ -0,0 +1,49 @@
+{
+  "Descripcion": "string",
+  "Periodo": "2020-05-11T15:52:53.557Z",
+  "NumeroAsiento": 0,
+  "FechaIngreso": "2020-05-11T15:52:53.557Z",
+  "ValorTotal": 0,
+  "TipoAsiento": "EGRESO",
+  "EstadoAsiento": "APROBADO",
+  "MovimientosContables": [
+    {
+      "Descripcion": "string",
+      "CodigoCuenta": "string",
+      "EsDebito": true,
+      "Valor": 0,
+      "CodigoCentroDeCosto": "string",
+      "CodigoSucursal": "string",
+      "MovimientosAuxiliares": [
+        {
+          "Folio": 0,
+          "CodigoSII": 0,
+          "EsDebito": true,
+          "FechaVencimiento": "2020-05-11T15:52:53.557Z",
+          "Valor": 0,
+          "RUTContraparteComercial": "string"
+        }
+      ],
+      "MovimientosBancarios": [
+        {
+          "EsDebito": true,
+          "Fecha": "2020-05-11T15:52:53.558Z",
+          "Valor": 0,
+          "Folio": 0,
+          "TipoMovimientoBancario": "string"
+        }
+      ],
+      "BoletasDeHonorarios": [
+        {
+          "Folio": 0,
+          "FechaEmision": "2020-05-11T15:52:53.558Z",
+          "Descripcion": "string",
+          "RUTContraparteComercial": "string",
+          "ValorNeto": 0,
+          "CodigoSII": 0,
+          "EsDebito": true
+        }
+      ]
+    }
+  ]
+}
