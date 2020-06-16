f9a626259d0a61ba1de0309acc902772ee3a8129
MauricioRochaNubox <52832925+MauricioRochaNubox@users.noreply.github.com>
2020-06-16 15:40:20 -0400

    Create emisión-boleta-info-producto.js

---
+++ emisión-boleta-info-producto.js
@@ -0,0 +1,27 @@
+"productos": [
+      {
+        "fechaEmision": "2020-06-16T08:36:14.4221255-04:00",
+        "folio": 5000,
+        "rutContraparte": "11111111-1",
+        "razonSocialContraparte": "venta23",
+        "giroContraparte": "Servicios",
+        "comunaContraparte": "Vitacura",
+        "direccionContraparte": "Candelaria Goyenechea 3820, Vitacura",
+        "emailContraparte": "mail@ejemplo.com",
+        "codigoSucursal": "RM",
+        "secuencia": 1,
+        "afecto": "SI",
+        "producto": "Privado",
+        "descripcion": "Servicio",
+        "cantidad": 1,
+        "precio": 150000,
+        "valor": 150000,
+        "codigoItem": "01",
+        "unidadMedida": "UNID",
+        "fechaVencimiento": "2020-06-16T08:36:14.4221255-04:00",
+        "codigoSIITipoDeServicio": "1",
+        "fechaPeriodoDesde": "2020-06-16T08:36:14.4221255-04:00",
+        "fechaPeriodoHasta": "2020-06-16T08:36:14.4221255-04:00",
+        "observacion": "Observación"
+      }
+    ]
