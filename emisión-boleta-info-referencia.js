60ba70b4300bea37c0070f06d9bcb10af0254328
MauricioRochaNubox <52832925+MauricioRochaNubox@users.noreply.github.com>
2020-06-16 15:47:13 -0400

    Create emisión-boleta-info-referencia.js

---
+++ emisión-boleta-info-referencia.js
@@ -0,0 +1,10 @@
+"documentoReferenciado": {
+      "tipo": 0,
+      "folio": 123456,
+      "secuencia": 1,
+      "tipoDocumentoReferenciado": 0,
+      "folioDocumentoReferenciado": 654321,
+      "fechaDocumentoReferenciado": "2020-06-16T08:36:14.4221255-04:00",
+      "motivoReferencia": 1000,
+      "glosa": "Glosa"
+}
