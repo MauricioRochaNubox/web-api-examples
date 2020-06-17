ad45708b241c1ff79296c27f804f1feaf1b13cf4
MauricioRochaNubox <52832925+MauricioRochaNubox@users.noreply.github.com>
2020-06-17 16:22:37 -0400

    emision-venta-js-jquery-request.js

---
+++ emision-venta-js-jquery-request.js
@@ -0,0 +1,14 @@
+var settings = {
+  "url": "https://api.nubox.com/Nubox.API.cert/factura/documento/15478788-7/1/13954729-2/1/emitir/ventaExtendido",
+  "method": "POST",
+  "timeout": 0,
+  "headers": {
+    "token": "d8d35079-3fe4-48bd-8b02-de05030a25d3",
+    "Content-Type": "application/json"
+  },
+  "data": JSON.stringify({"productos":[{"rutContraparte":"6606048-9","razonSocialContraparte":"El Gran Solar","giroContraparte":"Servicios","tipo":33,"folio":1,"secuencia":1,"fecha":"2020-06-16T08:36:14.4104329-04:00","afecto":"SI","producto":"producto de ejemplo","descripcion":null,"cantidad":1,"comunaContraparte":"","direccionContraparte":"Av Vitacura 3110","precio":70000,"valor":0,"ponderacionDescuento":0,"emailContraparte":"mail@ejemplo.com","tipoDeServicio":"","fechaPeriodoDesde":"","fechaPeriodoHasta":"","fechaVencimiento":"","codigoSucursal":"Cod 0001","vendedor":"Pedro Sanchez","codigoItem":"01","unidadMedida":"UNID","codigoIMP":"","montoIMP":0,"indicadorDeTraslado":"1","formaDePago":"1","medioDePago":"EF","terminosDePagoDias":"","terminosDePagoCodigo":"","comunaDestino":"Santiago","rutSolicitanteFactura":"9663691-1","productoCambioSujeto":"","cantidadMontoCambioSujeto":0,"tipoGlobalAfecto":"","valorGlobalAfecto":0,"tipoGlobalExento":"","valorGlobalExento":0,"precioCambioSujeto":0,"descuentoMonto":0,"rutTransportista":"14183701-k","rutChofer":"14183701-k","patente":"SVFV02","nombreChofer":"Juan Pereira","direccionDestino":"Santa Rosa 215","ciudadDestino":"Santiago","tipoDeDespacho":"","nombreDeContacto":"Lorena Álvarez","observacion":"Observación."}],"documentoReferenciado":{"tipo":0,"folio":2343,"secuencia":0,"tipoDocumentoReferenciado":0,"folioDocumentoReferenciado":34,"fechaDocumentoReferenciado":"2020-06-16T08:36:14.4104329-04:00","motivoReferencia":0,"glosa":"Glosa"}}),
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
