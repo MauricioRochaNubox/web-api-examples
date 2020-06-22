fe9c07f7008b4514ec4437a27e53aafe4942ca68
MauricioRochaNubox <52832925+MauricioRochaNubox@users.noreply.github.com>
2020-06-21 21:11:53 -0400

    Update pdf-compras-js-fetch-request.js

--- pdf-compras-js-fetch-request.js
+++ pdf-compras-js-fetch-request.js
@@ -1,14 +1,17 @@
-var settings = {
-  "url": "https://api.nubox.com/Nubox.API/factura/documento/15478788-7/1/1/FAC/1-9/pdf",
-  "method": "GET",
-  "timeout": 0,
-  "headers": {
-    "token": "898e879d-ccba-40c4-898c-5d4a97f8ed9c",
-    "Content-Type": "application/json"
-  },
-  "data": "\n\nhttps://api.nubox.com/Nubox.API/factura/documento/15478788-7/1/1/FAC/1-9/pdf",
+var myHeaders = new Headers();
+myHeaders.append("token", "898e879d-ccba-40c4-898c-5d4a97f8ed9c");
+myHeaders.append("Content-Type", "application/json");
+
+var raw = "\n\nhttps://api.nubox.com/Nubox.API/factura/documento/15478788-7/1/1/FAC/1-9/pdf";
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
 };
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
+fetch("https://api.nubox.com/Nubox.API/factura/documento/15478788-7/1/1/FAC/1-9/pdf", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
