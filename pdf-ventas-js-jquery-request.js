ce334d4ca5a6575324efe825a875a21fce3804ec
MauricioRochaNubox <52832925+MauricioRochaNubox@users.noreply.github.com>
2020-06-21 22:06:23 -0400

    Update pdf-ventas-js-jquery-request.js

--- pdf-ventas-js-jquery-request.js
+++ pdf-ventas-js-jquery-request.js
@@ -1,1 +1,12 @@
+var settings = {
+  "url": "https://api.nubox.com/Nubox.API/factura/documento/15478788-7/1/1/FAC/pdf",
+  "method": "GET",
+  "timeout": 0,
+  "headers": {
+    "token": "898e879d-ccba-40c4-898c-5d4a97f8ed9c"
+  },
+};
 
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
