6ecf17244c9dff419e22ddcebea03c3dd23e1003
MauricioRochaNubox <52832925+MauricioRochaNubox@users.noreply.github.com>
2020-06-22 00:00:51 -0400

    Create xml-compra-js-jquery-request.js

---
+++ xml-compra-js-jquery-request.js
@@ -0,0 +1,12 @@
+var settings = {
+  "url": "https://api.nubox.com/Nubox.API/factura/documento/15478788-7/compra/2020-01-02/33/0/1",
+  "method": "GET",
+  "timeout": 0,
+  "headers": {
+    "token": "898e879d-ccba-40c4-898c-5d4a97f8ed9c"
+  },
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
