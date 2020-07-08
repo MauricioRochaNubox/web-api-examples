1da14d03902c1c0288ab3512475261d904b2a637
MauricioRochaNubox <52832925+MauricioRochaNubox@users.noreply.github.com>
2020-07-08 18:46:22 -0400

    Create clientes-request-xhr-example.js

---
+++ clientes-request-xhr-example.js
@@ -0,0 +1,16 @@
+var data = JSON.stringify([{"Rut":"2-7","RazonSocial":"Juan Cliente","Giro":"EMPRESA DE SERVICIOS DE INFORMATICA","Acteco":"ACTIVIDADES DE ASESORAMIENTO EMPRESARIAL Y EN MATERIA DE GESTION","DireccionLegal":"Orinoco 90","ComunaLegalNombre":"Las Condes","Contacto":"Juan Contador","Email":"juan.contador@ejemplo.com","seEnviaPDF":1}]);
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function() {
+  if(this.readyState === 4) {
+    console.log(this.responseText);
+  }
+});
+
+xhr.open("POST", "https://api.nubox.com/Nubox.API/factura/1-9/1/clientes");
+xhr.setRequestHeader("token", "74d69e7e-8596-4ac6-8430-ed3c5da83bed");
+xhr.setRequestHeader("Content-Type", "application/json");
+
+xhr.send(data);
