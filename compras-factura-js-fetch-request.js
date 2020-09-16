511d6a2906220425850685a70162477d65d4c840
MauricioRochaNubox <52832925+MauricioRochaNubox@users.noreply.github.com>
2020-09-15 23:54:34 -0300

    Create compras-factura-js-fetch-request.js

---
+++ compras-factura-js-fetch-request.js
@@ -0,0 +1,40 @@
+var myHeaders = new Headers();
+myHeaders.append("token", "82bcbde5-4285-4ce1-8804-1bced937fd26");
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify(
+{
+   "NumeroSerie":1,
+   "Rut":"827-3",
+   "RazonSocialProveedor":"Beta API",
+   "CodigoSIITipoDoc":33,
+   "Folio":1,
+   "Fecha":"2020-04-14T00:00:00.000Z",
+   "FechaVencimiento":"2020-05-14T00:00:00.000Z",
+   "RutEmisor":"2-7",
+   "MontoExento":0,
+   "MontoNeto":34440,
+   "MontoIva":6544,
+   "MontoTotal":40984,
+   "Glosa":"prueba de inserción de compra desde nubox",
+   "MontoIvaNoRetenido":0,
+   "IVAUsoComun":0,
+   "Periodo":4,
+   "Anno":2020,
+   "TotalLey18211":0,
+   "TipoTransaccionCompra":null,
+   "NetoActivoFijo":0,
+   "IvaActivoFijo":0
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("https://api.nubox.com/nubox.api/compra", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
