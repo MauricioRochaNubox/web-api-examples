be247db2ce63e892586dd5545960196bfeb981db
MauricioRochaNubox <52832925+MauricioRochaNubox@users.noreply.github.com>
2020-09-15 23:57:47 -0300

    Create compras-factura-js-xhr-request.js

---
+++ compras-factura-js-xhr-request.js
@@ -0,0 +1,39 @@
+var data = JSON.stringify(
+{
+   "NumeroSerie":1,
+   "Rut":"827-3",
+   "RazonSocialProveedor":"Beta API",
+   "CodigoSIITipoDoc":33,
+   "Folio":1,
+   "Fecha":"2020-04-14T00:00:00.000Z",
+   "FechaVencimiento":"2020-05-14T00:00:00.000Z",
+   "RutEmisor":"16197839-6",
+   "MontoExento":0,
+   "MontoNeto":34440,
+   "MontoIva":6544,
+   "MontoTotal":40984,
+   "Glosa":"prueba de inserción de compra desde nubox",
+   "MontoIvaNoRetenido":0,
+   "IVAUsoComun":0,
+   "Periodo":4,
+   "Anno":2020,
+   "TotalLey18211":0,
+   "TipoTransaccionCompra":null,
+   "NetoActivoFijo":0,
+   "IvaActivoFijo":0
+});
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function() {
+  if(this.readyState === 4) {
+    console.log(this.responseText);
+  }
+});
+
+xhr.open("POST", "https://api.nubox.com/nubox.api/compra");
+xhr.setRequestHeader("token", "82bcbde5-4285-4ce1-8804-1bced937fd26");
+xhr.setRequestHeader("Content-Type", "application/json");
+
+xhr.send(data);
