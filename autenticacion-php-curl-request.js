dcea1efe1a3e6edad232facff2e70a52d240a086
MauricioRochaNubox <52832925+MauricioRochaNubox@users.noreply.github.com>
2020-11-10 15:32:36 -0300

    Update autenticacion-php-curl-request.js

--- autenticacion-php-curl-request.js
+++ autenticacion-php-curl-request.js
@@ -1,13 +1,22 @@
-require "uri"
-require "net/http"
+<?php
 
-url = URI("https://api.nubox.com/nubox.api/autenticar")
+$curl = curl_init();
 
-https = Net::HTTP.new(url.host, url.port);
-https.use_ssl = true
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.nubox.com/nubox.api/autenticar",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_HTTPHEADER => array(
+    "Authorization: Basic RjJPdzB6UURnUHJkOjRGVHVLZEt2"
+  ),
+));
 
-request = Net::HTTP::Post.new(url)
-request["Authorization"] = "Basic aWM3cFJISjFOUHJkOkZudldRc2VB"
+$response = curl_exec($curl);
 
-response = https.request(request)
-puts response.read_body
+curl_close($curl);
+echo $response;
